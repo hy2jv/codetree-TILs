d27edf8d728aec8fcd442b37a89c9b3a1d26108e
HyeJu Han <metaannotation@gmail.com>
2024-03-02 17:12:33 +0900

    Title: 정수 명령 처리 4, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240302/정수 명령 처리 4/process-numeric-commands-4.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+q = deque()
+for _ in range(int(input())):
+    command = input().split()
+    if (command[0] == 'push'):
+        q.append(int(command[1]))
+    elif command[0] == 'pop':
+        if len(q) == 0: print(-1)
+        else:
+            print(q.pop())
+    elif command[0] == 'size':
+        print(len(q))
+    elif command[0] == 'empty':
+        if len(q) == 0: print(1)
+        else: print(0)
+    elif command[0] == 'front':
+        if len(q) == 0: print(-1)
+        else: 
+            x = q.popleft()
+            print(x)
+            q.appendleft(x)
+    elif command[0] == 'back':
+        if len(q) == 0: print(-1)
+        else:
+            x = q.pop()
+            print(x)
+            q.appendleft(x)
