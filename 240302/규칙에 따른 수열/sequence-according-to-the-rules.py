8ec97c08a3f850e632c8c17e4626a31bd4969cb3
HyeJu Han <metaannotation@gmail.com>
2024-03-02 17:01:02 +0900

    Title: 규칙에 따른 수열, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240302/규칙에 따른 수열/sequence-according-to-the-rules.py
+++ 240302/규칙에 따른 수열/sequence-according-to-the-rules.py
@@ -3,9 +3,9 @@ arr = [0] * (n+1)
 arr[0] = 1
 arr[1] = 1
 arr[2] = 2
-for i in range(3, n+1):
+for idx in range(3, n+1):
     result = 0
-    for j in range(i+1):
-        result += (arr[j] * arr[n-j-1])
-    arr[i] = result
+    for i in range(n):
+        result += (arr[i] * arr[n-i-1])
+    arr[idx] = result
 print(arr[n])
