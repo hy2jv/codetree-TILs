1eec118f8816a442ba4ea7f62d7ba4f476a6fab0
HyeJu Han <metaannotation@gmail.com>
2024-03-02 17:03:37 +0900

    Title: 규칙에 따른 수열, Time: 178ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/규칙에 따른 수열/sequence-according-to-the-rules.py
+++ 240302/규칙에 따른 수열/sequence-according-to-the-rules.py
@@ -1,13 +1,11 @@
-def sequence(n):
-    if n == 0:
-        return 1
-    else:
-        t = [1]
-        for i in range(1, n+1):
-            total = 0
-            for j in range(i):
-                total += t[j] * t[i-j-1]
-            t.append(total)
-        return t[n]
-
-print(sequence(int(input())))
+n = int(input())
+arr = [0] * (n+1)
+arr[0] = 1
+arr[1] = 1
+arr[2] = 2
+for i in range(3, n+1):
+    result = 0
+    for j in range(i+1):
+        result += (arr[j] * arr[i-j-1])
+    arr[i] = result
+print(arr[n])
