56c02feca692c67a28591635100c9087fc8ff777
HyeJu Han <metaannotation@gmail.com>
2024-03-14 21:48:45 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240314/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
+++ 240314/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -6,6 +6,6 @@ searchChar = input()
 cnt, result = 0, 0
 for i in range(n):
     if searchChar in arr[i][0]:
-        cnt += arr[i].count(searchChar)
+        cnt += 1
         result += len(arr[i])
 print('%d %.2f' % (cnt, result/cnt))
